6cd7f75443d2fb319b4bd73239487c2794f230ab
Promess02 <miko20020921@gmail.com>
2023-08-25 22:26:17 +0200

    Revert "addded mySQL database and flyway"
    This reverts commit b2eaf0e43f9a62c5a3ddf8cc970d099d8d867edf.

--- src/main/resources/db/migration/V2__add_order.sql
+++
@@ -1,6 +0,0 @@
-create table book_order(
-    orderID int primary key auto_increment,
-    bookID bigint not null,
-    timestamp datetime not null,
-    foreign key (bookID) references book(id)
-);
